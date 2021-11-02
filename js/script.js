c29e43c51f3fed11d86cfbacce3dbc105f350bdb
Jubaer Habibee <38384488+rhjubi@users.noreply.github.com>
2021-11-02 22:32:22 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,126 @@
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let shoppingCart = document.querySelector('.shopping-cart');
+
+document.querySelector('#cart-btn').onclick = () =>{
+    shoppingCart.classList.toggle('active');
+    searchForm.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let icon = document.querySelector('.left-icon');
+
+document.querySelector('#back-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    signupForm.classList.remove('active');
+    forgotForm.classList.remove('active');
+}
+
+
+let icon1 = document.querySelector('.left-icon1');
+
+document.querySelector('#back-btn1').onclick = () =>{
+    loginForm.classList.toggle('active');
+    forgotForm.classList.remove('active');
+}
+
+let cancel = document.querySelector('.cancel');
+
+document.querySelector('#cancel').onclick = () =>{
+    loginForm.classList.remove('active');
+}
+
+let signupForm = document.querySelector('.signup-form');
+
+document.querySelector('#signup-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+    signupForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let forgotForm = document.querySelector('.forgot-form');
+
+document.querySelector('#forgot-btn').onclick = () =>{
+    loginForm.classList.remove('active');
+    forgotForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    loginForm.classList.remove('active');
+    navbar.classList.remove('active');
+}
+
+var swiper = new Swiper(".product-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
+
+var swiper = new Swiper(".review-slider", {
+    loop:true,
+    spaceBetween: 20,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    centeredSlides: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      768: {
+        slidesPerView: 2,
+      },
+      1020: {
+        slidesPerView: 3,
+      },
+    },
+});
